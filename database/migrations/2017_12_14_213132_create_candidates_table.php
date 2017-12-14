0e175abf133fa83abce7e67ee4dff6976d61aedf
Anderson Carlos Woss <anderson@woss.eng.br>
2017-12-14 21:36:51 -0200

    Criada a tabela de candidatos

---
+++ database/migrations/2017_12_14_213132_create_candidates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('email', 255);
+            $table->string('role', 255);
+            $table->string('linkedin', 255);
+            $table->string('avatar', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
